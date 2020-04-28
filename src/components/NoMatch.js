3bd288219006c6b9dd38788e244612fdb7346674
Alhassan Atama <komputarist@gmail.com>
2020-04-28 16:37:23 +0100

    localized the no match component

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
 import { Container, Header } from 'semantic-ui-react';
 
 export class NoMatch extends Component {
   render() {
     return (
       <Container textAlign="center">
-        <Header as="h3">No Match 404 Error</Header>
-        <p>Nothing to see here. Please use the menu to try again.</p>
+        <Header as="h3">
+          <FormattedMessage id="no.match.404" defaultMessage="No Match 404 Error" />
+        </Header>
+        <p>
+          <FormattedMessage
+            id="no.match.nothing.text"
+            defaultMessage="Nothing to see here. Please use the menu to try again."
+          />
+        </p>
       </Container>
     );
   }
